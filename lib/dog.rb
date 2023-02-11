5a1d53cfe9d4f5d75f056a7aaf6815ec910855e1
osallahstevie <osallahstevie@gmail.com>
2023-02-11 23:52:17 +0300

    completed better and setter lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-
-end
+    # setter method
+    def name=(value)
+        @name = value
+      end  
+    
+     def name
+        @name
+     end 
+    
+    def breed=(value)
+        @breed = value
+      end
+    
+      # getter method
+      def breed
+        @breed
+      end 
+    
+    end
